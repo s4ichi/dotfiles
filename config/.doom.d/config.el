c20bd3226176704788c67b8a56fa5e28347c0e32
s4ichi <s.wakeup31@gmail.com>
2019-03-10 06:07:31 +0900

    Set company configurations to enable auto-complete automatically

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -58,7 +58,9 @@
 
 ;; doom configurations
 (setq
- confirm-kill-emacs nil)
+ confirm-kill-emacs nil
+ company-idle-delay 0.2
+ company-minimum-prefix-length 2)
 
 ;; use-package configurations
 (def-package! doom-themes
