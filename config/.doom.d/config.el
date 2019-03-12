0df3262a2ba6266056cafaef17e471bcd768c0b3
s4ichi <s.wakeup31@gmail.com>
2019-03-12 16:34:21 +0900

    Change theme

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -68,9 +68,8 @@
   (doom-themes-enable-italic t)
   (doom-themes-enable-bold t)
   :custom-face
-  (doom-modeline-bar ((t (:background "#6272a4"))))
   :config
-  (load-theme 'doom-opera t)
+  (load-theme 'doom-sourcerer t)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
 
