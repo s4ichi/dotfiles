ecbea3ec9d07d75ba9adbb6369196017feff54d8
takamasa-saichi <takamasa-saichi@cookpad.com>
2019-03-13 13:52:31 +0900

    Support iam

--- config/.doom.d/+ruby.el
+++ config/.doom.d/+ruby.el
@@ -29,10 +29,12 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.iam$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (custom-set-variables '(enh-ruby-add-encoding-comment-on-save nil))
 (add-hook
