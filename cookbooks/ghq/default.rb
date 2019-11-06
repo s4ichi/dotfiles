170f9d8bb3528406322b3d09bf163d0d0c4afeb7
s4ichi <s.wakeup31@gmail.com>
2019-11-06 17:43:17 +0900

    Bump up version of ghq

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,6 +1,6 @@
 github_binary 'ghq' do
   repository 'motemen/ghq'
-  version 'v0.8.0'
+  version 'v0.12.8'
   archive "ghq_#{node[:os]}_amd64.zip"
-  binary_path "ghq"
+  binary_path "ghq_#{node[:os]}_amd64/ghq"
 end
