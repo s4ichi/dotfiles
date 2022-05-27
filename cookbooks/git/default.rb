6cb4e5752a8db3aa8e75acbb86d63130211a007c
takamasa-saichi <takamasa-saichi@cookpad.com>
2022-05-27 18:14:30 +0900

    Update gitconfig

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,4 +1,10 @@
 package 'git'
 
 ln '.gitconfig'
+ln '.gitconfig.user'
 ln '.gitignore'
+
+file "#{ENV['HOME']}/.gitconfig.local" do
+  not_if "test -e #{ENV['HOME']}/.gitconfig.local"
+  owner node[:user]
+end
