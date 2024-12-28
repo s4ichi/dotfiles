66e128b34ea40d63fe80ec2fac82a8b8ccf91b88
s4ichi <s.wakeup31@gmail.com>
2024-12-28 14:05:56 +0900

    Remove neotree

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -106,35 +106,6 @@
          company-minimum-prefix-length 1
          company-selection-wrap-around t))
 
-(use-package! neotree
-  :after
-  projectile
-  :commands
-  (neotree-show neotree-hide neotree-dir neotree-find)
-  :custom
-  (neo-theme 'nerd2)
-  :bind
-  ("<f9>" . neotree-projectile-toggle)
-  :preface
-  (defun neotree-projectile-toggle ()
-    (interactive)
-    (let ((project-dir
-           (ignore-errors
-                                 ;;; Pick one: projectile or find-file-in-project
-             (projectile-project-root)
-             ))
-          (file-name (buffer-file-name))
-          (neo-smart-open t))
-      (if (and (fboundp 'neo-global--window-exists-p)
-               (neo-global--window-exists-p))
-          (neotree-hide)
-        (progn
-          (neotree-show)
-          (if project-dir
-              (neotree-dir project-dir))
-          (if file-name
-              (neotree-find file-name)))))))
-
 (use-package! which-key
   :diminish which-key-mode
   :hook (After-Init . Which-Key-mode))
