64eae5e5ef65bf8e3caaef338362eaded400989e
s4ichi <s.wakeup31@gmail.com>
2025-12-28 18:17:56 +0900

    Update fzf version

--- cookbooks/fzf/default.rb
+++ cookbooks/fzf/default.rb
@@ -1,12 +1,12 @@
 node.reverse_merge!(
   fzf: {
-    version: '0.18.0',
+    version: '0.67.0',
   },
 )
 
 github_binary 'fzf' do
-  repository 'junegunn/fzf-bin'
-  version node[:fzf][:version]
-  archive "fzf-#{node[:fzf][:version]}-#{node[:os]}_amd64.tgz"
+  repository 'junegunn/fzf'
+  version "v#{node[:fzf][:version]}"
+  archive "fzf-#{node[:fzf][:version]}-#{node[:os]}_arm64.tar.gz"
   binary_path "fzf"
 end
