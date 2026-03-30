e3fc56efcca0f6d25983ee82511e4543d8a47074
s4ichi <s.wakeup31@gmail.com>
2026-03-30 16:49:32 +0900

    Add aws-accounts Alfred workflow and update installation to use zipped .alfredworkflow with hash-based change detection

--- cookbooks/alfred-workflows/default.rb
+++ cookbooks/alfred-workflows/default.rb
@@ -7,9 +7,11 @@ end
 workflows_src_dir = File.expand_path('..', __FILE__)
 Dir.glob("#{workflows_src_dir}/*/").each do |workflow_dir|
   workflow_name = File.basename(workflow_dir)
-  dest = "#{alfred_workflows_dir}/#{workflow_name}"
+  dest      = "#{alfred_workflows_dir}/#{workflow_name}.alfredworkflow"
+  hash_file = "#{alfred_workflows_dir}/.#{workflow_name}.sha"
+  hash_cmd  = "cd '#{workflow_dir}' && find . -type f -not -name .DS_Store | sort | xargs shasum | shasum | cut -c1-40"
 
-  execute "cp -r #{workflow_dir} #{dest}" do
-    not_if "test -d #{dest}"
+  execute "cd '#{workflow_dir}' && zip -r '#{dest}' . -x '*.DS_Store' && open '#{dest}' && #{hash_cmd} > '#{hash_file}'" do
+    not_if "test -f '#{hash_file}' && [ \"$(#{hash_cmd})\" = \"$(cat '#{hash_file}')\" ]"
   end
 end
